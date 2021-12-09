441ea3b672dd60dbc56a0820ca5916833d9edce0
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-09 12:55:49 +0700

    add balance to account info

--- client/src/scripts/account.js
+++ client/src/scripts/account.js
@@ -1,17 +1,19 @@
-import { initContract } from "./ethereum";
+import { initContract, getBalance } from "./ethereum";
 const Account = require("../contracts/Account.json");
 
 export async function getAccountInfo(address) {
   const accountMng = await initContract(Account);
   const isVerified = await accountMng.isVerified(address);
+  const balance = await getBalance(address);
+
   if (isVerified) {
     const uri = await accountMng.accountURI(address);
     const data = await fetch(uri).then((response) => response.json());
     const username = data.username;
     const thumbnail = data.thumbnail;
-    return { address, username, thumbnail, isVerified: true };
+    return { address, username, thumbnail, balance, isVerified: true };
   }
-  return { address, username: "Guest", thumbnail: "./img/author/author-6.jpg", isVerified: false };
+  return { address, username: "Guest", thumbnail: "./img/author/author-6.jpg", balance, isVerified: false };
 }
 
 export async function registerOrUpdate(uri) {
