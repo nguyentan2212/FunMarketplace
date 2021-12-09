f04928b9f06ce82c40a9db50c9ed2cfaab081ac8
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-09 12:56:28 +0700

    convert price value to wei

--- client/src/scripts/exchange.js
+++ client/src/scripts/exchange.js
@@ -1,12 +1,13 @@
-import { initContract, ZeroAddess, getCurrentAccount } from "./ethereum";
+import { initContract, ZeroAddess, getCurrentAccount, toWei } from "./ethereum";
 const Exchange = require("../contracts/Exchange.json");
 
 export async function sell(tokenAddress, tokenId, price) {
   const exchange = await initContract(Exchange);
   const seller = await getCurrentAccount();
+  const weiPrice = await toWei(price);
   //const signature = await signMintData(account, tokenAddress, uri);
 
-  const order = [seller, tokenAddress, tokenId, ZeroAddess, price, false];
+  const order = [seller, tokenAddress, tokenId, ZeroAddess, weiPrice, false];
 
   console.log(order);
   try {
