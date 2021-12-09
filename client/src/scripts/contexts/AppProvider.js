54dfcf380b16a6ab77014919761d69ef2a0cce7f
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-12-09 21:22:26 +0700

    nothing changed

--- client/src/scripts/contexts/AppProvider.js
+++ client/src/scripts/contexts/AppProvider.js
@@ -3,18 +3,29 @@ import React, { createContext, useState } from "react";
 export const AppContext = createContext();
 
 export function AppProvider({ children }) {
-  const [user, setUser] = useState({isLogin: false, username: null, address: null, thumbnail: null, balance: 0});
+  const [user, setUser] = useState({
+    isLogin: false,
+    username: null,
+    address: null,
+    avatar: null,
+    balance: 0
+  });
   const login = (data) => {
-    setUser({isLogin: true, username: data.username, address: data.address, thumbnail: data.thumbnail, balance: data.balance});
-  }
+    setUser({
+      isLogin: true,
+      username: data.username,
+      address: data.address,
+      avatar: data.avatar,
+      banner: data.banner,
+      balance: data.balance
+    });
+  };
 
   const logout = () => {
-    setUser(user => ({...user, isLogin: false}))
-  }
+    setUser((user) => ({ ...user, isLogin: false }));
+  };
 
   return (
-    <AppContext.Provider value={{user, setUser, login, logout}}>
-      {children}
-    </AppContext.Provider>
+    <AppContext.Provider value={{ user, setUser, login, logout }}>{children}</AppContext.Provider>
   );
 }
